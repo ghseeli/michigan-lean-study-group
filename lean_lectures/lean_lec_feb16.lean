e9762d54fe86a77e73ff7c9e37c846c224758b93
George H. Seelinger <ghseeli@gmail.com>
2026-02-16 20:01:50 -0500

    Added a brief overview of what was discussed today.

---
+++ lean_lectures/lean_lec_feb16.lean
@@ -0,0 +1,22 @@
+/-!
+# lean_lec_feb9
+
+Scratchpad / lecture file for Feb 9.
+-/
+
+/-
+## 1. A Crash Course on Type Theory by Patrick Lutz
+Slides available at https://docs.google.com/presentation/d/1rCUIwiDXAjgJETUytakntq0P4oQpf5HqYG8wMaX-j9c/edit?slide=id.p#slide=id.p
+
+## 2. Brief discussion of exercises
+There are some sample exercises in the `lean_exercises` folder in this repository that Calvin collected from various sources.
+Depending on your comfort level with Lean tactics, the recommended order is
+1. RealAnalysisWorld1.lean
+2. monotone.lean
+3. boolean_rings.lean
+4. N_choose_2.lean
+
+## 3. Consider goal for our group
+If you are interested in hearing more about a topic or presenting on something, please email Calvin or George.
+Also, be thinking about potential formalization projects to use as case-studies for practice with Lean.
+-/
