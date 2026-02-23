6003a325939b7d80f9883b0afddcafab4fcfeae9
George H. Seelinger <ghseeli@gmail.com>
2026-02-23 13:50:24 -0500

    Added scratchpad for today's meeting

---
+++ lean_lectures/lean_lec_feb23.lean
@@ -0,0 +1,21 @@
+import Mathlib
+
+/-!
+# lean_lec_feb23
+
+Scratchpad / lecture file for Feb 23.
+-/
+
+/-
+## 1. Discuss potential formalization projects
+1. General Brouwer Fixed Point Theorem (Calvin)
+
+## 2. Break up and do exercises
+Depending on your comfort level with Lean tactics, the recommended order is
+1. RealAnalysisWorld1.lean
+2. monotone.lean
+3. boolean_rings.lean
+4. N_choose_2.lean
+
+## 3. Challenges / what has been learned?
+-/
