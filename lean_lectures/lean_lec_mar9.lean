ebc1d746b28fcb4c573b18b597062cd9f1cffa52
George H. Seelinger <ghseeli@gmail.com>
2026-03-09 15:32:36 -0400

    Added file for today's study group meeting.

---
+++ lean_lectures/lean_lec_mar9.lean
@@ -0,0 +1,23 @@
+
+import Mathlib
+
+/-!
+# lean_lec_mar9
+
+Scratchpad / lecture file for Mar 9.
+-/
+
+/-
+## 1. Coxeter groups in Lean (Ming Lim)
+
+Ming presented on his approach to formalizing Coxeter groups in Lean with Mathlib.
+Following *Combinatorics of Coxeter Groups* by Bj\"orner--Brenti.
+
+
+Ming's Github repo: https://github.com/mlyean/lean-coxeter
+
+## 2. Stone representation theorem (Patrick Lutz)
+
+Patrick presented on the Stone representation theorem (If B is a Boolean algebra, there is a set X such that B embeds into the power set of X).
+
+-/
