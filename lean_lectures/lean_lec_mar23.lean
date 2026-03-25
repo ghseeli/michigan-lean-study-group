1c2ee4dd44a142a486eb6ff0c483838a706454f0
George H. Seelinger <ghseeli@gmail.com>
2026-03-25 10:47:46 -0400

    Added lecture file for last Monday's meeting.

---
+++ lean_lectures/lean_lec_mar23.lean
@@ -0,0 +1,25 @@
+/-!
+# lean_lec_mar23
+
+Scratchpad / lecture file for Mar 23.
+-/
+
+/-
+# Presentation on Latin squares in Lean (George)
+
+George presented his joint work on formalizing Latin squares in Lean and submitting the final result into Lean's Mathlib.
+
+Note, an important resource for this and generally learning about Lean/Mathlib is the Lean Zulip chat.
+
+The code and the pull request are available here: https://github.com/leanprover-community/mathlib4/pull/36698
+The associated Zulip chat: https://leanprover.zulipchat.com/#narrow/channel/144837-PR-reviews/topic/.2336698.20feat.28Combinatorics.2FEnumerative.29.3A.20Latin.20squares
+
+# Discussion of projects
+
+To jump start some of our projects, we discussed in two weeks' time having a longer "drop-in" meeting where people can co-work together on getting some Lean projects going.
+Having more time will allow us to get into technical details and people can come and go as they need during the session.
+
+*Tentative date*: Monday, April 6th.
+
+In preparation, if you have a project you want to work on, we ask that you have the mathematical portion of the project well-prepared, that is, make sure you have a detailed mathematical sketch of how the result(s) you want to prove are actually proven.
+-/
